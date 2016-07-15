a1381312d8d6410bf66741e10868bae2a6091c8b
华仔_01 <610898343@qq.com>
2016-07-15 09:59:55 +0800

    greendao 测试

---
+++ daogenerator/src/main/java/com/example/DaoMaster.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.greenrobot.greendao.generator.DaoGenerator;
+import org.greenrobot.greendao.generator.Entity;
+import org.greenrobot.greendao.generator.Schema;
+
+public class DaoMaster {
+    public static void main(String[] args) {
+
+        Schema schema = new Schema(1, "com.ganhuo.entity");
+        addGanhuo(schema);
+        schema.setDefaultJavaPackageDao("com.ganhuo.dao");
+        try {
+            new DaoGenerator().generateAll(schema,"C:\\Users\\123\\AndroidStudioProjects\\EasyApplication\\app\\src\\main\\java-gen");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void addGanhuo(Schema schema) {
+        Entity entity = schema.addEntity("Ganhuo");
+        entity.addIdProperty();//主键 int类型
+        entity.addStringProperty("createdAt");
+        entity.addStringProperty("desc");
+        entity.addStringProperty("publishedAt");
+        entity.addStringProperty("source");
+        entity.addStringProperty("type");
+        entity.addBooleanProperty("used");
+        entity.addStringProperty("who");
+    }
+}
