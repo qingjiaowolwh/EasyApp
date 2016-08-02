a0fe7d47804f9c91f71a8b8220cb30193d01cc88
华仔_01 <610898343@qq.com>
2016-08-02 17:25:42 +0800

    Book

--- app/src/main/java/com/edu/zum/easyapp/fragment/BookFragment.java
+++ app/src/main/java/com/edu/zum/easyapp/fragment/BookFragment.java
@@ -6,6 +6,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.ScaleAnimation;
@@ -63,28 +64,20 @@ public class BookFragment extends XRecyclerViewFragment {
 
                 bookIv.setImageResource(model);
 
-//                ObjectAnimator translationtop = AnimatorUtil.translationBottom(bookIv, mRecyclerView.getChildAt(1 + 1).getY() - mRecyclerView.getChildAt(position + 1).getY());
-//                ObjectAnimator translationright = AnimatorUtil.translationRight(bookIv, mRecyclerView.getChildAt(1 + 1).getX() - mRecyclerView.getChildAt(position + 1).getX());
-
-//                ObjectAnimator translationtop = ObjectAnimator.ofFloat(bookIv, "translationY", mRecyclerView.getChildAt(position + 1).getY(), bookIv.getY());
-//                ObjectAnimator translationright = ObjectAnimator.ofFloat(bookIv, "translationX", mRecyclerView.getChildAt(position + 1).getX(), bookIv.getX());
-//                ObjectAnimator scalex = AnimatorUtil.scaleX(bookIv, 1.5f);
-//                AnimatorSet animatorSet = new AnimatorSet();
-//                animatorSet.play(translationtop)
-//                        .with(translationright);
-//                animatorSet.play(scalex).with(translationright);
-//                animatorSet.playTogether(scalex, AnimatorUtil.scaleY(bookIv, 1.5f));
-//                animatorSet.start();
-
                 TranslateAnimation translateAnimation = new TranslateAnimation(mRecyclerView.getChildAt(position + 1).getX() - mRecyclerView.getChildAt(position).getWidth(), bookIv.getY(), mRecyclerView.getChildAt(position + 1).getY(), bookIv.getY());
                 ScaleAnimation scaleAnimation = new ScaleAnimation(1.0f, 1.5f, 1.0f, 1.5f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
 
                 AnimationSet animationSet = new AnimationSet(true);
                 animationSet.addAnimation(translateAnimation);
                 animationSet.addAnimation(scaleAnimation);
+                animationSet.setInterpolator(new AccelerateDecelerateInterpolator());
                 animationSet.setDuration(300);
                 animationSet.setFillAfter(true);
                 bookIv.startAnimation(animationSet);
+//                AlphaAnimation alphaAnimation=new AlphaAnimation(1.0f,0.0f);
+//                alphaAnimation.setDuration(300);
+//                alphaAnimation.setFillAfter(true);
+//                mRecyclerView.startAnimation(alphaAnimation);
             }
         });
     }
