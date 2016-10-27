a4e8bd24f9f6c8bfee5516ee5043435f63c36e71
华仔_01 <610898343@qq.com>
2016-10-27 14:57:56 +0800

    环信整成了类库

---
+++ easeui/src/main/java/easemob/easeuix/widget/chatrow/ChatRowEvaluation.java
@@ -0,0 +1,77 @@
+package easemob.easeuix.widget.chatrow;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+
+import com.easemob.chat.EMMessage;
+import com.easemob.easeui.R;
+import com.easemob.easeui.widget.chatrow.EaseChatRow;
+
+import org.json.JSONObject;
+
+import easemob.helpdeskdemo.Constant;
+import easemob.helpdeskdemo.DemoHelper;
+import easemob.helpdeskdemo.ui.ChatFragment;
+import easemob.helpdeskdemo.ui.SatisfactionActivity;
+
+public class ChatRowEvaluation extends EaseChatRow{
+	
+	Button btnEval;
+
+	public ChatRowEvaluation(Context context, EMMessage message, int position, BaseAdapter adapter) {
+		super(context, message, position, adapter);
+	}
+
+	@Override
+	protected void onInflatView() {
+		if (DemoHelper.getInstance().isEvalMessage(message)) {
+			inflater.inflate(message.direct == EMMessage.Direct.RECEIVE ? R.layout.em_row_received_satisfaction
+					: R.layout.em_row_sent_satisfaction, this);
+		}
+	}
+
+	@Override
+	protected void onFindViewById() {
+		btnEval = (Button) findViewById(R.id.btn_eval);
+		
+	}
+
+	@Override
+	protected void onUpdateView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void onSetUpView() {
+		try {
+			final JSONObject jsonObj = message.getJSONObjectAttribute(Constant.WEICHAT_MSG);
+			if(jsonObj.has("ctrlType")&&!jsonObj.isNull("ctrlType")){
+				btnEval.setEnabled(true);
+				btnEval.setText("立即评价");
+				btnEval.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						((Activity)context).startActivityForResult(new Intent(context, SatisfactionActivity.class)
+								.putExtra("msgId", message.getMsgId()), ChatFragment.REQUEST_CODE_EVAL);
+					}
+				});
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void onBubbleClick() {
+		
+	}
+
+}
---
+++ easeui/src/main/java/easemob/helpdeskdemo/ui/ChatActivity.java
@@ -0,0 +1,82 @@
+package easemob.helpdeskdemo.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.easemob.easeui.R;
+
+import easemob.helpdeskdemo.Constant;
+import easemob.helpdeskdemo.utils.HelpDeskPreferenceUtils;
+
+
+/**
+ * 聊天页面，需要fragment的使用{@link EaseChatFragment}
+ */
+
+/**
+ *
+ */
+public class ChatActivity extends BaseActivity {
+
+    public static ChatActivity activityInstance;
+    private ChatFragment chatFragment;
+    String toChatUsername;
+
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.em_activity_chat);
+        activityInstance = this;
+        // 聊天人或群id
+        toChatUsername = HelpDeskPreferenceUtils.getInstance(this).getSettingCustomerAccount();
+        // 可以直接new EaseChatFratFragment使用
+        chatFragment = new ChatFragment();
+        Intent intent = getIntent();
+        intent.putExtra(Constant.EXTRA_USER_ID, toChatUsername);
+        intent.putExtra(Constant.EXTRA_SHOW_USERNICK, true);
+        // 传入参数
+        chatFragment.setArguments(intent.getExtras());
+        getSupportFragmentManager().beginTransaction().add(R.id.container, chatFragment).commit();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        activityInstance = null;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        // 点击notification bar进入聊天页面，保证只有一个聊天页面
+        String username = intent.getStringExtra("userId");
+        if (toChatUsername.equals(username))
+            super.onNewIntent(intent);
+        else {
+            finish();
+            startActivity(intent);
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        chatFragment.onBackPressed();
+    }
+
+//    public String getToChatUsername() {
+//        return toChatUsername;
+//    }
+//
+//	public void sendTextMessage(String txtContent){
+//		chatFragment.sendTextMessage(txtContent);
+//	}
+
+    public void sendRobotMessage(String txtContent, String menuId) {
+        chatFragment.sendRobotMessage(txtContent, menuId);
+    }
+
+
+
+}
